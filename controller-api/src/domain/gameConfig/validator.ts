7afdf2543111b384229c90a4c7d4432f50a99ba7
AladdineDev <aaldn.dev@gmail.com>
2024-07-22 02:29:50 +0200

    refactor: zod

--- controller-api/src/domain/gameConfig/validator.ts
+++ controller-api/src/domain/gameConfig/validator.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 import { ButtonType } from './index';
 
-export const GameConfigValidator = z.strictObject({
+export const GameConfigValidator = z.object({
   name: z.string(),
   buttonMapping: z.record(z.nativeEnum(ButtonType))
 });
